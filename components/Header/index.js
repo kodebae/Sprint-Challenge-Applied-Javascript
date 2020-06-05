24b29afba4980b10aa8cc1b9da95866f7734cc30
kodebae <karmenuopx@outlook.com>
2020-06-05 13:39:52 -0400

    Finished step one and header creation

--- components/Header/index.js
+++ components/Header/index.js
@@ -3,12 +3,28 @@
 // Write a function that returns the markup you see below:
 //
 //  <div class="header">
-//    <span class="date">SMARCH 28, 2019</span>
+//    <span class="date">MARCH 28, 2019</span>
 //    <h1>Lambda Times</h1>
 //    <span class="temp">98°</span>
 //  </div>
 //
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
+function Header() {
+    const div = document.createElement("div");
+    const span = document.createElement("span");
+    const h1 = document.createElement("h1");
 
-function Header() {}
+    // found a typo and changed 'SMARCH' to 'MARCH" in HTML and function code
+
+    span.textContent = "MARCH 28, 2019"
+    h1.textContent = "Lambda Times";
+    span2.textContent = "98° ";
+
+    header.appendChild(div);
+    header.appendChild(span);
+    header.appendChild(h1);
+    header.appendChild(span2);
+
+    return header;
+}
