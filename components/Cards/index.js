c3eb7cc585cd52f7f71cd6176a41c952f2b270c4
kodebae <karmenuopx@outlook.com>
2020-06-08 13:59:28 -0400

    pushing recent changes to GH

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -18,3 +18,64 @@
 // </div>
 //
 // Use your function to create a card for each of the articles and add the card to the DOM.
+
+/**
+ *! creating the card function  
+ */
+
+let myCardMakerFunc = function(headlineInfo, name, photo) {
+    let myCard = document.createElement("div");
+    let myHeadline = document.createElement("div");
+    let myAuthor = document.createElement("div");
+    let myImg = document.createElement("div");
+    let myImgSrc = document.createElement("img");
+    let myAuthorName = document.createElement("span")
+
+    /**
+     * !append the child containers
+     */
+
+    myCard.appendChild(myHeadline);
+    myCard.appendChild(myAuthor);
+    myAuthor.appendChild(myImg);
+    myImg.appendChild(myImgSrc);
+    myAuthor.appendChild(myAuthorName);
+
+
+    /**
+     * !adding the class names 
+     */
+
+    myCard.className = "card";
+
+};
+
+
+
+
+axios.get("https://lambda-times-backend.herokuapp.com/articles")
+    .then(response => {
+
+        const myBootstrapArticle = response.data.articles.bootstrap;
+
+        const myJavascriptArticle = response.data.articles.javascript;
+
+        const myJqueryArticle = response.data.articles.jquery;
+
+        const myNodeArticle = response.data.articles.node;
+
+        const myTechnologyArticle = response.data.articles.technology;
+
+
+        response.data.article.forEach(item => {
+            const myCards = document.createElement("div");
+            myCards.className = "cards-container";
+            const myHeadline = document.createElement(`${headline}`);
+
+        })
+    })
+
+.catch(error => {
+    console.log("There is an error with the API", error);
+
+}, []);
